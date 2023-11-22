83be5512f02ad214e1da1a23f49073ab96dda4a3
Toven <toventungul@yahoo.com>
2023-11-22 12:00:53 -0800

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { useState, useEffect, React } from 'react';
+import { Nav, Header, Main, Footer } from './components';
+import {Routes, Route, useLocation  } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     Homepage
-    </div>
+    <div>Little Lemon</div>
+    // <Header/>
+    // <Nav/>
+    // <Main/>
+    // <Footer/>
   );
 }
 
